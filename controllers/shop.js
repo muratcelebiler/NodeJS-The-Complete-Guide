e7a201796cb3b1875b78a7a492421965556c84b4
Murat Çelebiler <muratcelebiler@teknasyon.com>
2021-09-06 23:31:29 +0300

    product detail view added

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,9 +3,13 @@ const Product = require('../models/product');
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
 
-  console.log(prodId);
-  
-  res.redirect('/');
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
 };
 
 exports.getProducts = (req, res, next) => {

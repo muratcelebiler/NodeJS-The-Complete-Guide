18789586f6ce085d4c190b1dcf4f8aac93c2cfa0
Murat Çelebiler <muratcelebiler@teknasyon.com>
2021-09-11 11:53:59 +0300

    fetching product detail with database

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,15 +2,21 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;
+  const productId = req.params.productId;
 
-  Product.findById(prodId, product => {
+  Product.findById(productId)
+  .then(([product]) => {
+    // product değişkeni içerisindeki birinci item içerisinde veritabanından dönen veriler mevcut.
+    // Biz burada pop() methodu ile birinci elementi çağırıyoruz. Yani product[0] değerini çağırıyoruz.
+    product = product.pop();
+
     res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
       path: '/products'
     });
-  });
+  })
+  .catch(err => console.log('getProduct db error: ', err));
 };
 
 exports.getProducts = (req, res, next) => {
@@ -22,7 +28,7 @@ exports.getProducts = (req, res, next) => {
       path: '/products'
     });
   })
-  .catch(err => console.log('getProduct db error: ', err));
+  .catch(err => console.log('getProducts db error: ', err));
 };
 
 exports.postCart = ((req, res, next) => {

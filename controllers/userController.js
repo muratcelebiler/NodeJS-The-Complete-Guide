203f1ab209a0a23b960b0bdb6525ff19bfcd6764
Murat Çelebiler <muratcelebiler@teknasyon.com>
2021-09-22 11:31:02 +0300

    user create method added into the model - validator message response updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,24 @@
+// Models
+const User = require('../models/user');
+
 // Post -> create
 exports.create = (req, res, next) => {
-    res.status(200).json({"success": true});
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email
+    });
+
+    user.save()
+        .then(user => {
+            res.status(200).json({
+                success: true,
+                user: user
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                success: false,
+                error: error
+            });
+        });
 };
--- requests/validator.js
+++ requests/validator.js
@@ -5,7 +5,10 @@ module.exports = (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({
+        success: false, 
+        errors: errors.array() 
+      });
     }
 
     next();
